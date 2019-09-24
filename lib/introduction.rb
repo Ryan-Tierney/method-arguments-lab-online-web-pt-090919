0717c206fb8f2a0714ba25f0610a0c1ce0d1ae24
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-24 17:07:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 def introduction(name)""
   puts "Hi, my name is #{name}."
   
+=======
+def introduction(name)
+  puts "Hi, my name is Josh"
+>>>>>>> 86855ff8bb5884f674d73962677cf01304f045e9
 end
 
 def introduction_with_language(name, language)
